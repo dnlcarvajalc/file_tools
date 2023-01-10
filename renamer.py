9a5d1112e82234eed8d069b5c5805e598ec3f125
Daniel Carvajal Correa <118780631+dnlcarvajalc@users.noreply.github.com>
2023-01-10 16:13:12 -0500

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -2,11 +2,12 @@ import os
 import shutil
 
 PATH_FOLDER = '<folder_path>'
+EXTENSION = '.jpg'
 
 def files_list(location: str):
     _filelist = []
     for root, dirs, files in os.walk(location):
-        _filelist = _filelist + [os.path.join(root, file) for file in files if file.endswith('.jpg')]
+        _filelist = _filelist + [os.path.join(root, file) for file in files if file.endswith(EXTENSION)]
     return _filelist
 
 def rename_files(filelist: list):
@@ -15,7 +16,7 @@ def rename_files(filelist: list):
         root_folder_name = file.rsplit("/",1)[0]
         if not os.path.isdir(f"{root_folder_name}/renamed"):
             os.system(f"mkdir {root_folder_name}/renamed")
-        shutil.copy(file, f'{root_folder_name}/renamed/{i}.jpg')
+        shutil.copy(file, f'{root_folder_name}/renamed/{i}{EXTENSION}')
         i += 1
 
 if __name__ == "__main__":
