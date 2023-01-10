c9dcaecaadec8fc662fd6ba7927396d4fd1d7fa4
Daniel Carvajal Correa <118780631+dnlcarvajalc@users.noreply.github.com>
2023-01-10 12:33:58 -0500

    Added renamer util

---
+++ renamer.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+PATH_FOLDER = '<folder_path>'
+
+def files_list(location: str):
+    _filelist = []
+    for root, dirs, files in os.walk(location):
+        _filelist = _filelist + [os.path.join(root, file) for file in files if file.endswith('.jpg')]
+    return _filelist
+
+def rename_files(filelist: list):
+    i = 0
+    for file in filelist:
+        root_folder_name = file.rsplit("/",1)[0]
+        if not os.path.isdir(f"{root_folder_name}/renamed"):
+            os.system(f"mkdir {root_folder_name}/renamed")
+        shutil.copy(file, f'{root_folder_name}/renamed/{i}.jpg')
+        i += 1
+
+if __name__ == "__main__":
+    filelist = files_list(PATH_FOLDER)
+    rename_files(filelist)
